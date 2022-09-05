de3a922364808be7b8afa254eb5cc514f4aadafe
Alessio Tugnoli <77077281+yougnoli@users.noreply.github.com>
2022-09-05 12:49:38 +0200

    Update create-table-single-json.sql

--- code/final/create-table-single-json.sql
+++ code/final/create-table-single-json.sql
@@ -1,14 +1,18 @@
-drop table twe.single_json
-
+-- create table: insertion date is default, j column accepts the whole json fromm the pipeline
 create table twe.single_json (
 	id int identity(1,1) not null primary key
 	,j nvarchar(max)
 	,insertion_date date default getdate()
 )
+;
+go
 
 select * from twe.single_json
-truncate table twe.single_json
+;
+go
 
+
+-- test query to see the final output from the j column
 select
 	id
 	,j
@@ -20,8 +24,10 @@ select
 	,convert(nvarchar(max), json_value(j, '$.long_tweet')) as long_tweet
 from 
 	twe.single_json
+;
+go
 
-
+-- the output works, I can alter the table and create calculated column
 alter table twe.single_json
 	add publication_date as convert(date, json_value(j, '$.publication_date'))
 	,modulo as convert(varchar(5), json_value(j, '$.module'))
@@ -29,7 +35,10 @@ alter table twe.single_json
 	,topic as convert(varchar(20), json_value(j, '$.topic')) 
 	,first_tweet as convert(nvarchar(300), json_value(j, '$.first_tweet')) 
 	,long_tweet as convert(nvarchar(max), json_value(j, '$.long_tweet')) 
+;
+go
 
+-- test from the python code
 select
 	first_tweet
 	,long_tweet
